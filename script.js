e437da78b848b1178723d8512e507f4daef6c069
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-06-22 18:48:31 +0400

    add single round logic

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 // Computer choice logic
-console.log("Hello World");
+
 const getComputerChoice = () => {
   const choices = ["rock", "paper", "scissors"];
   const choice = choices[Math.floor(Math.random() * 3)];  
   return choice;
 }
 
-getComputerChoice();
+
 
 //Human choice
 
 const getHumanChoice = () => {
   let choice = prompt("Rock, paper or scissors? Please enter: ");  
-  choice = choice.toLowerCase();
-  console.log(choice);
+  choice = choice.toLowerCase();  
   return choice;
 }
 
-getHumanChoice();
+
 
 // Score variables
 
 let humanScore = 0;
 let computerScore = 0;
 
+// Single round logic
+
+const playRound = (humanChoice, computerChoice) => {
+  let roundResult= "";
+  if (humanChoice === "rock" && computerChoice === "paper"
+    || humanChoice === "paper" && computerChoice === "scissors"
+    || humanChoice === "scissors" && computerChoice === "rock") {
+    roundResult = `You lose! ${computerChoice} beats ${humanChoice}`;
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors"
+    || humanChoice === "paper" && computerChoice === "rock"
+    || humanChoice === "scissors" && computerChoice === "paper") {
+    roundResult = `You win! ${humanChoice} beats ${computerChoice}`;
+    humanScore++;
+  } else {
+    roundResult = "Draw!";
+  }
+  console.log(roundResult);  
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
